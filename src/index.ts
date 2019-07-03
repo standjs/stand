441532ee26ef06e068d9b05d0d58c531650f7b9d
haixu.bo <haixu.bo@zhaopin.com.cn>
2019-07-03 17:02:48 +0800

    feat:   initial not done

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-const a: number = 2
+import Koa, { Context } from 'koa'
+import Router from 'koa-router'
+
+const app = new Koa()
+const router = new Router()
+
+router.get('/', (ctx: Context, next) => {
+  ctx.response.type = 'text/html'
+  ctx.response.body = '<div>lalala</div>'
+})
+
+app.use(router.routes())
+
+app.listen('3000', () => {
+  console.log('server has start')
+})
